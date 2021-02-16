3ef63c7d7eb1d77b4028fcc3db999aad4ab5f1d9
HexaLopata <alekseygasyuk50@gmail.com>
2021-02-16 17:07:30 +0300

    Теперь количество пуль за выстрел может быть разным

--- Assets/Scripts/Weapons/StandaloneWeapon.cs
+++ Assets/Scripts/Weapons/StandaloneWeapon.cs
@@ -8,14 +8,19 @@ public class StandaloneWeapon : Weapon
     [Space]
     [Header("Angle range in degrees")]
     [SerializeField] private float _angleRange;
+    [Header("Bullets per one shot")]
+    [SerializeField] private int _bulletCount = 1;
 
     public override float Shoot(float timeBetweenShots, float angle, Vector2 position)
     {
         Bullet bullet;
         if(timeBetweenShots >= _fireRate)
         {
-            bullet = Instantiate(_bullet, position, new Quaternion(0, 0, angle, 0));
-            bullet.transform.rotation = Quaternion.Euler(0, 0, angle + Random.Range(-_angleRange, _angleRange));
+            for(int i = 0; i < _bulletCount; i++)
+            {
+                bullet = Instantiate(_bullet, position, new Quaternion(0, 0, angle, 0));
+                bullet.transform.rotation = Quaternion.Euler(0, 0, angle + Random.Range(-_angleRange, _angleRange));
+            }
 
             return 0;
         }
